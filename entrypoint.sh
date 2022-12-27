ebb2338496d99ab87380fa050e524670b7d938b4
bradd evans <braddevans123@gmail.com>
2022-12-27 20:14:04 +0000

    should use pnpm

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,22 @@
 cd /home/container
 
 if [ -z "$(ls -A /home/container)" ]; then
-   echo "directory found to be Empty";
-   git clone https://github.com/braddevans/pter-nodejs-pnpm-egg.git /tmp/egg
-   ls -lash /tmp/egg/
-   cp /tmp/egg/container/* . -v
+  echo "directory found to be Empty"
+  git clone https://github.com/braddevans/pter-nodejs-pnpm-egg.git /tmp/egg
+  ls -lash /tmp/egg/
+  cp /tmp/egg/container/* . -v
 else
-   echo "Not Empty"
+  echo "Not Empty"
 fi
 
 node -v
 
 SHOULD_INSTALL=true
+SHOULD_USE_PNPM=false
 NPM_COMMAND="i"
 NODE_START_FILE="run server"
 
-function run_npm() {
+function run_pnpm() {
   NPM_PACKAGE_INSTALLER="pnpm {{NPM_COMMAND}}"
   MODIFIED_NPM_PACKAGE_INSTALLER=$(eval echo $(echo ${NPM_PACKAGE_INSTALLER} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
   echo ":/home/container$ ${MODIFIED_NPM_PACKAGE_INSTALLER}"
@@ -25,6 +26,14 @@ function run_npm() {
   ${MODIFIED_NPM_PACKAGE_INSTALLER}
 }
 
+function run_npm() {
+  NPM_PACKAGE_INSTALLER="npm {{NPM_COMMAND}}"
+  MODIFIED_NPM_PACKAGE_INSTALLER=$(eval echo $(echo ${NPM_PACKAGE_INSTALLER} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+  echo ":/home/container$ ${MODIFIED_NPM_PACKAGE_INSTALLER}"
+
+  # Run the npm command
+  ${MODIFIED_NPM_PACKAGE_INSTALLER}
+}
 
 function run_server() {
   NPM_START="pnpm {{NODE_START_FILE}}"
@@ -35,9 +44,14 @@ function run_server() {
   ${MODIFIED_NPM_START}
 }
 
-if [ "$SHOULD_INSTALL" = "true" ]; then
-  # run npm command
-  run_npm
+if [ "${SHOULD_INSTALL}" = "true" ]; then
+  if [ "$SHOULD_USE_PNPM" = "true" ]; then
+    # run pnpm command
+    run_pnpm
+  elif [ "$SHOULD_USE_PNPM" = "false" ]; then
+    # run node command
+    run_npm
+  fi
 fi
 
 # run server
